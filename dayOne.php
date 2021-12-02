91daf6673b7a0496a540222954e03bb094ba2405
Ryan Beiden <ryanbeiden@ryanbeismacbook.lan>
2021-12-02 09:40:21 -0600

    day 1 / part 2
    https://adventofcode.com/2021/day/1#part2

--- dayOne.php
+++ dayOne.php
@@ -17,12 +17,35 @@ class DayOne
         $measurementCount = 0;
 
         for ($i = 1; $i < count($measurements); $i++) {
-            $previousMeasurement = $i;
-            if ($measurements[$i] > $measurements[$previousMeasurement - 1]) {
+            $measurementIndex = $i;
+            if ($measurements[$i] > $measurements[$measurementIndex - 1]) {
                 $measurementCount += 1;
             }
         }
 
         return $measurementCount;
+    }
+
+    /**
+     * The measurements array is the provided input:
+     * https://adventofcode.com/2021/day/1/input
+     *
+     * @param array $measurements
+     *
+     * @return int
+     */
+    public function partTwo(array $measurements) : int
+    {
+        $measurementSumCount = 0;
+
+        for ($i = 1; $i < count($measurements) - 2; $i++) {
+            if (($measurements[$i] + $measurements[$i + 1] + $measurements[$i + 2])
+                > ($measurements[$i] + $measurements[$i - 1] + $measurements[$i + 1])
+            ) {
+                $measurementSumCount += 1;
+            }
+        }
+
+        return $measurementSumCount;
     }
 }
